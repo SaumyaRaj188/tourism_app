0ab233d609b0328d25f3480aac6e8cecff43b1b3
SaumyaRaj188 <saumyaraj188@gmail.com>
2023-12-15 02:38:31 +0530

    Initial commit

---
+++ lib/searchBox.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MySearchBox extends StatefulWidget {
+  const MySearchBox({super.key});
+
+  @override
+  State<MySearchBox> createState() => _MySearchBoxState();
+}
+
+class _MySearchBoxState extends State<MySearchBox> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/searchPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MySearchPage extends StatefulWidget {
+  const MySearchPage({super.key});
+
+  @override
+  State<MySearchPage> createState() => _MySearchPageState();
+}
+
+class _MySearchPageState extends State<MySearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(children: <Widget>[]),
+    );
+  }
+}
